9d3d4f7fb8a980886110aa90112354499b868fbe
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-09-01 07:59:15 +0000

    [start]

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  
+  post 'lists' => 'lists#create'
+  
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
